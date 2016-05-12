3f660199cceb4a2c59f2bd33467ca2f7c47ada37
Hannah <hannah.tinkler@gmail.com>
2016-05-12 13:58:17 +0100

    Update the ability to view on the buttons

--- resources/views/partials/page_options.blade.php
+++ resources/views/partials/page_options.blade.php
@@ -1,8 +1,13 @@
-<button class="btn btn-default"><a href="/page/edit/{{ $page->id }}">Edit</a></button>
-<form action="/page/{{ $page->id }}" method="POST">
-    {!! csrf_field() !!}
-    {!! method_field('DELETE') !!}
-    <button type="submit" id="delete-page-{{ $page->id }}" class="btn btn-default">
-        <i class="fa fa-btn fa-trash"></i> Delete
-    </button>
-</form>
+@if($page->created_by == $user->id || $user->curator)
+    <form action="/page/{{ $page->id }}" method="POST">
+        <div class="btn-group pull-right">
+            <a class="btn btn-default" href="/page/edit/{{ $page->id }}"><i class="fa fa-pencil"></i> Edit</a>
+        
+            {!! csrf_field() !!}
+            {!! method_field('DELETE') !!}
+            <button type="submit" id="delete-page-{{ $page->id }}" class="btn btn-default">
+                <i class="fa fa-btn fa-trash"></i> Delete
+            </button>
+        </div>
+    </form>
+@endif
