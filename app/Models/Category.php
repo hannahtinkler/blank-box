c129f29f66a6d8c06de43c1239b2f0dcb05daf8d
Hannah <hannah.tinkler@gmail.com>
2016-08-25 16:40:02 +0100

    order chapters by title then order for chapters

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -24,6 +24,6 @@ class Category extends Model
 
     public function chapters()
     {
-        return $this->hasMany('App\Models\Chapter')->orderBy('title', 'order');
+        return $this->hasMany('App\Models\Chapter')->orderBy('title')->orderBy('order');
     }
 }
