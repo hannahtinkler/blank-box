c3a8d1b88648f6004dcc6485504951c49ece52a6
Hannah <hannah.tinkler@gmail.com>
2016-08-25 16:20:22 +0100

    Fix authors being able to edit

--- resources/views/partials/page_options.blade.php
+++ resources/views/partials/page_options.blade.php
@@ -1,22 +1,19 @@
-@if($page->created_by == $user->id || $user->curator)
+@if($user->curator)
     <div class="m-t-sm btn-group pull-right page-options">
-        @if ($user->curator)
-            @if($page->approved === null)
-                <div class="m-t-sm btn-group pull-right">
-                    <a class="btn btn-default" href="/curation/new/approve/{{ $page->id }}"><i class="fa fa-check"></i> Approve</a>
-                    <a class="btn btn-default" href="/curation/new/reject/{{ $page->id }}"><i class="fa fa-remove"></i> Reject</a>
-                </div>
-            @endif
-            <form action="/pages/{{ $page->id }}" method="POST">
-                {!! csrf_field() !!}
-                {!! method_field('DELETE') !!}
-                <button type="submit" id="delete-page-{{ $page->id }}" class="btn btn-default">
-                    <i class="fa fa-trash-o"></i> Delete
-                </button>
-            </form>
-            <a class="btn btn-default" href="/pages/edit/{{ $page->id }}"><i class="fa fa-pencil"></i> Edit</a>
+        @if($page->approved === null)
+            <div class="m-t-sm btn-group pull-right">
+                <a class="btn btn-default" href="/curation/new/approve/{{ $page->id }}"><i class="fa fa-check"></i> Approve</a>
+                <a class="btn btn-default" href="/curation/new/reject/{{ $page->id }}"><i class="fa fa-remove"></i> Reject</a>
+            </div>
         @endif
-        
+        <form action="/pages/{{ $page->id }}" method="POST">
+            {!! csrf_field() !!}
+            {!! method_field('DELETE') !!}
+            <button type="submit" id="delete-page-{{ $page->id }}" class="btn btn-default">
+                <i class="fa fa-trash-o"></i> Delete
+            </button>
+        </form>
+        <a class="btn btn-default" href="/pages/edit/{{ $page->id }}"><i class="fa fa-pencil"></i> Edit</a>
     </div>
 @else
     <div class="m-t-sm btn-group pull-right">
