42673a333e0a8703a50b6c2a749b08229c552be0
Hannah <hannah.tinkler@gmail.com>
2016-09-13 12:28:08 +0100

    Add some hover text to the category heading

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -36,7 +36,7 @@
                      <div class="dropdown profile-element">
                         <a data-toggle="dropdown" class="dropdown-toggle" href="table_data_tables.html#">
                             <span class="clear">
-                                <span class="text-mutedblock" title="Switch Categories" title="You are currently exploring the {{ $current['category']->title }} category">
+                                <span class="text-mutedblock" title="You are currently exploring the {{ $current['category']->title }} category. Click to switch categories. ">
                                 {{ $current['category']->title }}
                                 @if($categories->count() > 1)
                                     <b class="caret"></b></span>
