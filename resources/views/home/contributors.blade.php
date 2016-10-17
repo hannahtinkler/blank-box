671fe62fc717250bee2c2585b353f4e3a474fc5c
Hannah <hannah.tinkler@gmail.com>
2016-10-17 09:33:44 +0100

    Repo link on contribute page

--- resources/views/home/contributors.blade.php
+++ resources/views/home/contributors.blade.php
@@ -12,7 +12,7 @@
 <ul>
     <li class="m-t-md">Add new pages whenever you find yourself making notes that might help someone else, think of something important that isn't already covered here, or just whenever you have a spare 10 minutes. (Okay, maybe that last one is unlikely. But you could make time maybe?)</li>
     <li class="m-t-md">Volunteer as a curator, meaning you'd help maintain the quality and readability of the pages and suggested edits that are submitted to the knowledge base.</li>
-    <li class="m-t-md">Fork the Black Box repositiory and submit all kinds of information-centric paraphernalia and code-iness via pull request. If you'd like to be able to refer to <?php echo env('APP_NAME', 'Black Box'); ?> for something it does not yet offer, you could build it.</li>
+    <li class="m-t-md">Fork <a target="_blank" href="https://github.com/hannahtinkler/black-box">the Black Box repositiory</a> and submit all kinds of information-centric paraphernalia and code-iness via pull request. If you'd like to be able to refer to <?php echo env('APP_NAME', 'Black Box'); ?> for something it does not yet offer, you could build it.</li>
 </ul>
 
 <h2 class="m-t-xl">Current Contributors</h2>
