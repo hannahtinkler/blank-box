80ed0f87411bfad7227015b2a1fab0de6f126033
Hannah <hannah.tinkler@gmail.com>
2016-11-14 17:44:00 +0000

    Moved a thing

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,6 +12,23 @@
 
             <div class="all-comments">
 
+                @if(date('Y-m-d') < '2016-12-24')
+                    <div class="vertical-timeline-block">
+                        <div class="vertical-timeline-icon navy-bg">
+                            <div class="icon-circle"><i class="feed-icon fa fa-exclamation"></i></div>
+                        </div>
+
+                        <div class="vertical-timeline-content">
+                            <h4><strong>Super Very Important News:</strong></h4>
+                            <p>Only {{ $daysTilXmas }} Days Until Christmas!</p>
+                            <span class="vertical-date">
+                                <small>{{ date('jS M Y') }}</small><br />
+                                <small>17:11</small>
+                            </span>
+                        </div>
+                    </div>
+                @endif
+                
                 @if(date('Y-m-d') < '2016-12-14')
                     <div class="vertical-timeline-block">
                         <div class="vertical-timeline-icon navy-bg">
@@ -28,22 +45,6 @@
                             </ul>
                             <span class="vertical-date">
                                 <small>14th Nov 2016</small><br />
-                                <small>17:11</small>
-                            </span>
-                        </div>
-                    </div>
-                @endif
-                @if(date('Y-m-d') < '2016-12-24')
-                    <div class="vertical-timeline-block">
-                        <div class="vertical-timeline-icon navy-bg">
-                            <div class="icon-circle"><i class="feed-icon fa fa-exclamation"></i></div>
-                        </div>
-
-                        <div class="vertical-timeline-content">
-                            <h4><strong>Super Very Important News:</strong></h4>
-                            <p>Only {{ $daysTilXmas }} Days Until Christmas!</p>
-                            <span class="vertical-date">
-                                <small>{{ date('jS M Y') }}</small><br />
                                 <small>17:11</small>
                             </span>
                         </div>
