cfd93825608925463a17ce23c64a9c0fc10c1db2
Hannah <hannah.tinkler@gmail.com>
2016-11-14 16:42:31 +0000

    Oops

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -272,8 +272,11 @@
 
 
 <!-- Main scripts -->
+@if(env(APP_THEME) == 'christmas-skin')
+    <script src="/js/snowstorm.js"></script>
+@endif
+
 <script src="/js/jquery-2.1.1.js"></script>
-<script src="/js/snowstorm.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 <script src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>
 <script src="/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
