b0f15056e7e3be076a16f720f5959907d2628032
Hannah Tinkler <hannah.tinkler@mrbandfriends.co.uk>
2017-08-29 17:48:24 +0100

    Reduce required characters for title

--- app/Http/Requests/PageRequest.php
+++ app/Http/Requests/PageRequest.php
@@ -21,7 +21,7 @@ class PageRequest extends FormRequest
     {
         return [
             'chapter_id' => 'required|integer|exists:chapters,id',
-            'title' => 'required|min:5',
+            'title' => 'required|min:3',
             'description' => 'required|min:10',
         ];
     }
